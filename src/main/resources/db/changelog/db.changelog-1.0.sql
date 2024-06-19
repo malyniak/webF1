fa6aff73aa391cc17c26d6dc3411d383524ada86
malyniak <malyniakvolodiaaa@gmil.com>
2024-06-19 22:03:02 +0300

    add changelog master, create tables

---
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -0,0 +1,45 @@
+--liquibase formatted sql
+
+--changeset Volodymyr Malyniak:1
+CREATE TABLE IF NOT EXISTS Driver (
+                                    id INT PRIMARY KEY AUTO_INCREMENT,
+                                    firstname VARCHAR(255) NOT NULL,
+                                    lastname VARCHAR(255) NOT NULL,
+                                    birthdate DATE,
+                                    nationality VARCHAR(255),
+                                    height INT,
+                                    weight INT,
+                                    car_number INT,
+                                    FOREIGN KEY (car_number) REFERENCES Car(number),
+                                    team INT,
+                                    FOREIGN KEY (team) REFERENCES Team(id)
+);
+
+--changeset Volodymyr Malyniak:2
+CREATE TABLE IF NOT EXISTS Car (
+                                       id INT PRIMARY KEY AUTO_INCREMENT,
+                                       number INT UNIQUE NOT NULL ,
+                                       team VARCHAR(255) NOT NULL ,
+                                       FOREIGN KEY (team) references Team(id)
+);
+
+--changeset Volodymyr Malyniak:3
+CREATE TABLE IF NOT EXISTS Team (
+                                     id INT PRIMARY KEY AUTO_INCREMENT,
+                                     boss VARCHAR(255) UNIQUE NOT NULL,
+                                     name VARCHAR(255) UNIQUE NOT NULL ,
+                                     engine VARCHAR NOT NULL ,
+
+);
+
+--changeset Volodymyr Malyniak:4
+CREATE TABLE IF NOT EXISTS Contract (
+                                    id INT PRIMARY KEY AUTO_INCREMENT,
+                                    driver INT,
+                                    team INT,
+                                    salary INT,
+                                    last_year INT,
+                                    FOREIGN KEY (driver) REFERENCES Driver(id),
+                                    FOREIGN KEY (team) REFERENCES Team(id)
+);
+
